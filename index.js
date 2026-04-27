39c383615860a59ed964edcdb0be2a66e6f6ff6f
iury-ai <iury@legare.com.br>
2026-04-27 10:24:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
   res.send('Servidor de ligações rodando!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
